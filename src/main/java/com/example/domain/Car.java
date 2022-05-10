4f06f87980f87084a32dabf100d5d5188ce80e7d
suzuki shota <shota.suzuki@rakus-partners.co.jp>
2022-05-10 16:25:43 +0900

    second commit

--- src/main/java/com/example/domain/Car.java
+++ src/main/java/com/example/domain/Car.java
@@ -2,6 +2,15 @@ package com.example.domain;
 
 public class Car {
 	private String name;
+	private int speed;
+	
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
 
 	public String getName() {
 		return name;
